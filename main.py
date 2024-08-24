02c781d912979d90fec28bbfef1ef599a549adc4
Vitor Hugo <vitorangelozi2013@gmail.com>
2024-08-23 23:37:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello World!</>"
-def print_ascii_art():
-    print('''
+print('''
     *******************************************************************************
               |                   |                  |                     |
      _________|________________.=""_;=.______________|_____________________|_______
